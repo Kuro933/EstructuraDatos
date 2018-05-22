593571ad53c9833189de788254f091eee3470422
Federico <33671243+Kuro933@users.noreply.github.com>
2018-05-22 18:16:40 -0300

    Update ArbolHeap.java
    lo cambio directamente ya que el que tenia era para int nada mas y ahora esta con Comparable

--- Estructura/conjuntista/src/conjuntistas/ArbolHeap.java
+++ Estructura/conjuntista/src/conjuntistas/ArbolHeap.java
@@ -4,49 +4,40 @@ public class ArbolHeap {
 
 	private final static int TAM = 20;
 	private int ultimo;
-	private int[] heap;
+	private Comparable[] heap;
 
 	public ArbolHeap() {
 		this.ultimo = 0;
-		this.heap = new int[TAM - 1];
+		this.heap = new Comparable[TAM - 1];
 	}
 
 	public boolean insertar(Comparable elem) {
 		boolean exito = false;
 
 		if (this.ultimo < TAM) {
+
 			this.ultimo++;
-			this.heap[ultimo]=elem;
+			this.heap[ultimo] = elem;
 			exito = true;
-			acomodar(heap,ultimo);
+			acomodar(ultimo);
 		}
 		return exito;
 	}
-	private void acomodar(Comparable[] arr, int posicion){
-		if(posicion/2!=0){
-		Comparable padre = this.heap[posicion/2];
-		Comparable hijo = this.heap[posicion];
-		while(padre.compareTo(hijo)>0 && posicion>1){
-			System.out.println("ingresa while");
-			System.out.println("posicion:" + (posicion/2));
-			System.out.println("padre:" + padre.toString());
-			System.out.println("hijo: " + hijo.toString());
-			
-			
-			Comparable aux=padre;
-			this.heap[posicion/2] = hijo;
-			this.heap[posicion] = aux;
-			
-			posicion=posicion/2;
-			padre=arr[posicion/2];
-			hijo = arr[posicion];
-			System.out.println("antes de salir del while");
-			System.out.println("posicion: " + posicion);
-			System.out.println("padre:" + padre.toString());
-			System.out.println("hijo: " + hijo.toString());
-			System.out.println("----");
+
+	private void acomodar(int posicion) {
+		if (posicion / 2 != 0) {
+			Comparable padre = this.heap[posicion / 2];
+			Comparable hijo = this.heap[posicion];
+			while (padre.compareTo(hijo) > 0 && posicion > 1) {
+				Comparable aux = padre;
+				this.heap[posicion / 2] = hijo;
+				this.heap[posicion] = aux;
+				posicion = posicion / 2;
+				if (posicion > 1) {
+					padre = this.heap[posicion / 2];
+					hijo = this.heap[posicion];
+				}
 			}
-		System.out.println("salio del while");
 		}
 	}
 
@@ -64,18 +55,18 @@ public class ArbolHeap {
 
 	private void hacerBajar(int posicion) {
 		int hijoMenor;
-		int temp = this.heap[posicion];
+		Comparable temp = this.heap[posicion];
 		boolean salir = false;
 
 		while (!salir) {
 			hijoMenor = posicion * 2;
 			if (hijoMenor <= this.ultimo) {
 				if (hijoMenor < this.ultimo) {
-					if (this.heap[hijoMenor + 1] < this.heap[hijoMenor]) {
+					if (this.heap[hijoMenor + 1].compareTo(this.heap[hijoMenor]) < 0) {
 						hijoMenor++;
 					}
 				}
-				if (this.heap[hijoMenor] < temp) {
+				if (this.heap[hijoMenor].compareTo(temp) < 0) {
 					this.heap[posicion] = this.heap[hijoMenor];
 					posicion = hijoMenor;
 				} else {
@@ -88,33 +79,42 @@ public class ArbolHeap {
 		this.heap[posicion] = temp;
 	}
 
-	public int recuperarCima() {
-		int elem=Integer.MIN_VALUE;
-		if(!esVacio()){
-			elem=this.heap[1];	
+	public Comparable recuperarCima() {
+		Comparable elem = null;
+		if (!esVacio()) {
+			elem = this.heap[1];
 		}
 		return elem;
 	}
 
-	public boolean esVacio(){
-		return this.ultimo==0;
+	public boolean esVacio() {
+		return this.ultimo == 0;
 	}
-	
+
 	public String toString() {
 		String arbol = "";
 		int posicion = 1;
-		while(posicion <= TAM){
-		if (!esVacio()) {
+		while (posicion < this.ultimo) {
 			arbol += "Padre: " + this.heap[posicion];
 			arbol += "\n";
-			arbol += "Hijo izquierdo: " + this.heap[posicion*2] + " ";
-			arbol += "Hijo derecho: " + this.heap[(posicion*2)+1] + " ";
+			arbol += "Hijo izquierdo: " + this.heap[posicion * 2] + " ";
+			arbol += "Hijo derecho: " + this.heap[(posicion * 2) + 1] + " ";
 
-			}
 			arbol += "\n";
 			posicion++;
 		}
+
 		return arbol;
 	}
+	/*este String es por si lo quieren como un arreglo
+	un ejemplo de como quedaria seria |1|2|3|4|
+	 public String toString() {
+	 String arbol = "";
+	 arbol += "|";
+	 for (int i = 1; i <= this.ultimo; i++) {
+	 arbol += this.heap[i] + "|";
+	 }
+	 return arbol;
+	*/ }
 
 }
